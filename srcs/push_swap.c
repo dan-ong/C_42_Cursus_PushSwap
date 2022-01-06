d3c51da76bebb561dd8e8c4bacbefe1481228e12
dong <dong@F3C015.42adel.org.au>
2022-01-06 13:56:58 +1030

    updated at 42

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dong <dong@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 16:01:32 by dong              #+#    #+#             */
-/*   Updated: 2021/12/24 23:47:54 by dong             ###   ########.fr       */
+/*   Updated: 2022/01/06 13:54:43 by dong             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,28 @@ int	main(int argc, char **argv)
 	while (!(numlst_sorted(list_a, 'a') && num_lstsize(list_b) == 0))
 	{
 		push_swapper(&list_a, &list_b);
+		// if (num_lstsize(list_b) == 1)
+		// 	return (0);
 	}
 	return (0);
 }
 
 void	push_swapper(t_numlist **list_a, t_numlist **list_b)
 {
+	// num_lstprint(*list_a);
+	// ft_putstr_fd("\n", 1);
+	// ft_putnbr_fd(num_lstmin(*list_a), 1);
+	// ft_putstr_fd("\nlistb size: ", 1);
+	// ft_putnbr_fd(num_lstsize(*list_b), 1);
+	// if (num_lstsize(*list_b) == 1)
+	// 	return ;
 	if (numlst_sorted(*list_a, 'a') && numlst_sorted(*list_b, 'd'))
 	{
 		num_lstpush(list_b, list_a);
 		ft_putstr_fd("pa\n", 1);
 	}
 	else if ((*list_a)->num == num_lstmin(*list_a)
-		&& (*list_a)->num < num_lstavg(*list_a, *list_b))
+		&& (*list_a)->num <= num_lstavg(*list_a, *list_b))
 	{
 		num_lstpush(list_a, list_b);
 		ft_putstr_fd("pb\n", 1);
